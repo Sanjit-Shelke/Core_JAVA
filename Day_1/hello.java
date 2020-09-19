a73a1d0429d32ca524007930592354071dd9176b
Sanjit-Shelke <sanjitshelke@gmail.com>
2020-09-19 09:09:32 +0530

    Day_1 Assignment Completed

---
+++ Day_1/hello.java
@@ -0,0 +1,7 @@
+class hello {
+    public static void main(final String[] args) {
+        
+        System.out.println("Hello Java");
+        
+    }
+}
---
+++ Day_1/variables.java
@@ -0,0 +1,16 @@
+class variables {
+    public static void main(String[] args) {
+		int a=10;               // 4 bytes
+		long b=20;              // 8 bytes
+		float c=(float)20.10;   // 4 bytes
+		double d=10.20;         // 8 bytes
+		char e='J';             // 2 bytes
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+	}
+}
+		
